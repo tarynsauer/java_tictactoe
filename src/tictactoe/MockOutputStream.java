9e6d4f64715d6d514e42acdd50954914bca921f0
Taryn Sauer <taryn.sauer@gmail.com>
2014-01-09 21:46:17 -0600

    Remove unnecessary variable from MockOutputStream

--- src/tictactoe/MockOutputStream.java
+++ src/tictactoe/MockOutputStream.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
  * Created by Taryn on 1/8/14.
  */
 public class MockOutputStream extends OutputStream {
-    ArrayList<String> printCallHistory = new ArrayList<String>();
 
     @Override
     public void write(int i) throws IOException {
