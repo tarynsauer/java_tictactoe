948c6f2dab05acb6a5c016049199ea513d12b50e
Taryn Sauer <taryn.sauer@gmail.com>
2014-01-31 10:07:22 -0600

    Add new main class file

--- src/tictactoe/PlayGame.java
+++ src/tictactoe/PlayGame.java
@@ -1,7 +1,14 @@
 package tictactoe;
 
+import java.io.IOException;
+
 /**
  * Created by Taryn on 1/31/14.
  */
 public class PlayGame {
+
+    public static void main(String[] args) throws IOException {
+      GameRunner runner = new GameRunner();
+      runner.run();
+    }
 }
